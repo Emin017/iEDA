9dff098401a699cc88062e0e97f7e23c318fe017
Qiming Chu <cchuqiming@gmail.com>
2025-03-19 13:26:19 +0800

    build(nix): add nix flake for iEDA
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

---
+++ nix/pkgs/iedaClang/default.nix
@@ -0,0 +1,98 @@
+{
+  lib,
+  clangStdenv,
+  llvmPackages,
+  cmake,
+  ninja,
+  flex,
+  bison,
+  zlib,
+  tcl,
+  boost,
+  eigen,
+  yaml-cpp,
+  libunwind,
+  glog,
+  gtest,
+  gflags,
+  metis,
+  gmp,
+  python3,
+  onnxruntime,
+  iir-rust,
+  sdf-parse,
+  spef-parser,
+  vcd-parser,
+  verilog-parser,
+  liberty-parser,
+}:
+clangStdenv.mkDerivation {
+  pname = "iedaClang";
+  version = "0.1.0";
+
+  src =
+    with lib.fileset;
+    toSource {
+      root = ./../../..;
+      fileset = unions [
+        ./../../..
+      ];
+    };
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    flex
+    bison
+    python3
+    tcl
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "CMD_BUILD" true)
+    (lib.cmakeBool "SANITIZER" false)
+    (lib.cmakeBool "BUILD_STATIC_LIB" false)
+  ];
+
+  preConfigure = ''
+    cmakeFlags+=" -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:FILEPATH=$out/bin -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:FILEPATH=$out/lib"
+  '';
+
+  buildInputs = [
+    iir-rust
+    sdf-parse
+    spef-parser
+    vcd-parser
+    verilog-parser
+    liberty-parser
+    gtest
+    glog
+    gflags
+    boost
+    onnxruntime
+    eigen
+    yaml-cpp
+    libunwind
+    metis
+    gmp
+    tcl
+    zlib
+    llvmPackages.openmp
+  ];
+
+  postInstall = ''
+    # Tests rely on hardcoded path, so they should not be included
+    rm $out/bin/*test $out/bin/*Test $out/bin/test_* $out/bin/*_app
+  '';
+
+  enableParallelBuild = true;
+
+  meta = {
+    description = "Open-source EDA infracstructure and tools from Netlist to GDS for ASIC design";
+    homepage = "https://gitee.com/oscc-project/iEDA";
+    license = lib.licenses.mulan-psl2;
+    maintainers = with lib.maintainers; [ xinyangli ];
+    mainProgram = "iEDA";
+    platforms = lib.platforms.linux;
+  };
+}
