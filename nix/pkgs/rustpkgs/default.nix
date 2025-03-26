369e3e91cbc9b82d5e565b6df5f6f0e15dbd5926
Qiming Chu <cchuqiming@gmail.com>
2025-03-26 16:12:24 +0800

    refactor(nix): consolidate rust packages into rustpkgs
    1. Removes individual package.nix files for rust packages
    2. Creates a new rustpkgs/default.nix that contains all rust package
    definitions
    3. Updates scope.nix to reference packages from the new rustpkgs
    
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

---
+++ nix/pkgs/rustpkgs/default.nix
@@ -0,0 +1,97 @@
+{ rustPlatform }:
+let
+  mkRustpkgs = _: p: rustPlatform.buildRustPackage p;
+in
+(builtins.mapAttrs mkRustpkgs {
+  iir-rust = {
+    pname = "iir-rust";
+  version = "0.1.3";
+  src = ./../../../src/operation/iIR/source/iir-rust/iir;
+  cargoBuildType = "release";
+
+  cargoLock = {
+    lockFile = ./../../../src/operation/iIR/source/iir-rust/iir/Cargo.lock;
+  };
+
+  cargoHash = "sha256-xyNaUTPvU21yOdkQq8vdnHCyLzcpDAFAje0R/gDqliU=";
+
+  doCheck = false;
+
+  nativeBuildInputs = [ rustPlatform.bindgenHook ];
+  };
+  liberty-parser = {
+    pname = "liberty-parser";
+  version = "0.1.3";
+  src = ./../../../src/database/manager/parser/liberty/lib-rust/liberty-parser;
+  cargoBuildType = "release";
+
+  cargoLock = {
+    lockFile = ./../../../src/database/manager/parser/liberty/lib-rust/liberty-parser/Cargo.lock;
+  };
+
+  cargoHash = "sha256-nRIOuSz5ImENvKeMAnthmBo+2/Jy5xbM66xkcfVCTMI=";
+
+  doCheck = false;
+  nativeBuildInputs = [ rustPlatform.bindgenHook ];
+  };
+  sdf_parse = {
+    pname = "sdf_parse";
+  version = "0.1.0";
+  src = ./../../../src/database/manager/parser/sdf/sdf_parse;
+  cargoBuildType = "release";
+
+  cargoLock = {
+    lockFile = ./../../../src/database/manager/parser/sdf/sdf_parse/Cargo.lock;
+  };
+
+  cargoHash = "sha256-PORA/9DDIax4lOn/pzmi7Y8mCCBUphMTzbBsb64sDl0=";
+
+  nativeBuildInputs = [ rustPlatform.bindgenHook ];
+  };
+  spef-parser = {
+    pname = "spef-parser";
+  version = "0.2.5";
+  src = ./../../../src/database/manager/parser/spef/spef-parser;
+  cargoBuildType = "release";
+
+  cargoLock = {
+    lockFile = ./../../../src/database/manager/parser/spef/spef-parser/Cargo.lock;
+  };
+
+  cargoHash = "sha256-o83rK7laEUMAeNIUZH0sARe1MUK9j+vJIDLJ8TOSOxU=";
+
+  nativeBuildInputs = [ rustPlatform.bindgenHook ];
+  };
+  vcd_parser = {
+    pname = "vcd_parser";
+  version = "0.1.0";
+  src = ./../../../src/database/manager/parser/vcd/vcd_parser;
+  cargoBuildType = "release";
+
+  cargoLock = {
+    lockFile = ./../../../src/database/manager/parser/vcd/vcd_parser/Cargo.lock;
+  };
+
+  cargoHash = "sha256-xcfVzDrnW4w3fU7qo6xzSQeIH8sEbEyzPF92F5tDcAk=";
+
+  doCheck = false;
+
+  nativeBuildInputs = [ rustPlatform.bindgenHook ];
+  };
+  verilog-parser = {
+    pname = "verilog-parser";
+  version = "0.1.0";
+  src = ./../../../src/database/manager/parser/verilog/verilog-rust/verilog-parser;
+  cargoBuildType = "release";
+
+  cargoLock = {
+    lockFile = ./../../../src/database/manager/parser/verilog/verilog-rust/verilog-parser/Cargo.lock;
+  };
+
+  cargoHash = "sha256-ooxY8Q8bfD+klBGfpTDD3YyWptEOGGHDoyamhjlSNTM=";
+
+  doCheck = false;
+
+  nativeBuildInputs = [ rustPlatform.bindgenHook ];
+  };
+})
--- nix/scope.nix
+++ nix/scope.nix
@@ -3,16 +3,17 @@ lib.makeScope newScope (
   scope:
   let
     findPackage = path: scope.callPackage ./pkgs/${path}/package.nix { };
+    rustpkgs = scope.callPackage ./pkgs/rustpkgs { };
   in
   {
     ieda = findPackage "ieda";
     iedaClang = scope.callPackage ./pkgs/iedaClang { };
     glog = findPackage "glog";
-    iir-rust = findPackage "iir-rust";
-    liberty-parser = findPackage "liberty-parser";
-    sdf-parse = findPackage "sdf-parse";
-    spef-parser = findPackage "spef-parser";
-    vcd-parser = findPackage "vcd-parser";
-    verilog-parser = findPackage "verilog-parser";
+    iir-rust = rustpkgs.iir-rust;
+    liberty-parser = rustpkgs.liberty-parser;
+    sdf-parse = rustpkgs.sdf_parse;
+    spef-parser = rustpkgs.spef-parser;
+    vcd-parser = rustpkgs.vcd_parser;
+    verilog-parser = rustpkgs.verilog-parser;
   }
 )
